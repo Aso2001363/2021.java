e6cf96133a62f8317e474ba643c5e07a1072d16e
Aso2001363 <82856655+Aso2001363@users.noreply.github.com>
2021-07-30 20:02:54 +0900

    Add files via upload

---
+++ Main37.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main37 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("***************************");
+        System.out.println("         戦艦ゲーム         ");
+        System.out.println("***************************");
+        System.out.println("爆弾のX座標を入力してください");
+        String x = scanner.next();
+        System.out.println(x);
+        System.out.println("爆弾のY座標を入力してください");
+        String y = scanner.next();
+        System.out.println(y);
+
+    }
+}
